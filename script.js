1fb9a5dd6ccbd6c98866834f86a53130ff760d04
Tavarina <fabi.ana_deoliveira@outlook.com>
2023-11-18 02:19:55 -0300

    criando escopo da pagina

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector('.carousel-container');
+    const slides = document.querySelectorAll('.slide');
+    let index = 0;
+
+    function updateCarousel() {
+        container.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    function nextSlide() {
+        if (index < slides.length - 1) {
+            index++;
+        } else {
+            index = 0;
+        }
+        updateCarousel();
+    }
+
+    function prevSlide() {
+        if (index > 0) {
+            index--;
+        } else {
+            index = slides.length - 1;
+        }
+        updateCarousel();
+    }
+
+    setInterval(nextSlide, 3000); 
+
+});
